7e4da509413ff375c5a45ba8149199e6ae06c916
Eymar Flores <e.floresmercado0784@gmail.com>
2026-07-30 12:43:25 -0600

    Implementado zoom y exploración de Masaya en el mapa

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,5 @@
 const map = L.map('map').setView([12.8, -85.0], 7);
+const departamentos = L.layerGroup().addTo(map);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution:'OpenStreetMap'
@@ -7,32 +8,20 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 /* MANAGUA */
 
 L.marker([12.1364, -86.2514])
-.addTo(map)
+.addTo(departamentos)
 .bindPopup("<b>Managua</b><br>Capital de Nicaragua");
 
 /* GRANADA */
 
 L.marker([11.9344, -85.9560])
-.addTo(map)
+.addTo(departamentos)
 .bindPopup("<b>Granada</b><br>Ciudad colonial y turística");
 
 /* LEÓN */
 
 L.marker([12.4350, -86.8780])
-.addTo(map)
+.addTo(departamentos)
 .bindPopup("<b>León</b><br>Cultura, historia y volcanes");
-
-/* OMETEPE */
-
-L.marker([11.5077, -85.6220])
-.addTo(map)
-.bindPopup("<b>Ometepe</b><br>Isla volcánica y turismo natural");
-
-/* SAN JUAN DEL SUR */
-
-L.marker([11.2529, -85.8705])
-.addTo(map)
-.bindPopup("<b>San Juan del Sur</b><br>Playas y turismo internacional");
 
 // Grupo de lugares turísticos de Masaya
 const lugaresMasaya = L.layerGroup();
@@ -76,42 +65,51 @@ L.marker([11.9698, -86.0978])
 L.marker([11.9730, -86.0965])
     .bindPopup('<b>🎨 Talleres de Artesanía</b>')
     .addTo(lugaresMasaya);
-
 // Marcador del departamento
 L.marker([11.9744, -86.0942])
-    .addTo(map)
+    .addTo(departamentos)
     .bindPopup("<b>📍 Masaya</b><br>Haz clic para explorar sus lugares turísticos.")
     .on("click", function () {
-        map.setView([11.9744, -86.0942], 11);
+
+        // Ocultar los departamentos
+        map.removeLayer(departamentos);
+
+        // Acercamiento con animación
+        map.flyTo([11.9744, -86.0942], 12, {
+            duration: 2
+        });
+
+        // Mostrar los lugares turísticos
         lugaresMasaya.addTo(map);
+
     });
-    
+        
 /* RIVAS */
 L.marker([11.4372, -85.8263])
-.addTo(map)
+.addTo(departamentos)
 .bindPopup("<b>Rivas</b><br>Puerta de la Isla de Ometepe");
 
 /* MATAGALPA */
 L.marker([12.9256, -85.9175])
-.addTo(map)
+.addTo(departamentos)
 .bindPopup("<b>Matagalpa</b><br>La Perla del Septentrión");
 
 /* JINOTEGA */
 L.marker([13.0910, -86.0014])
-.addTo(map)
+.addTo(departamentos)
 .bindPopup("<b>Jinotega</b><br>Ciudad de las Brumas");
 
 /* CHINANDEGA */
 L.marker([12.6294, -87.1311])
-.addTo(map)
+.addTo(departamentos)
 .bindPopup("<b>Chinandega</b><br>Tierra del Volcán San Cristóbal");
 
 /* RÍO SAN JUAN */
 L.marker([11.2586, -84.7770])
-.addTo(map)
+.addTo(departamentos)
 .bindPopup("<b>Río San Juan</b><br>Naturaleza y aventura");
 
 /* COSTA CARIBE */
 L.marker([12.1500, -83.7500])
-.addTo(map)
+.addTo(departamentos)
 .bindPopup("<b>Costa Caribe</b><br>Cultura y playas del Caribe");
